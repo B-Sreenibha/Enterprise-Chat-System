82887da1371044b3351283db632873a17598a139
B-Sreenibha <51774387+B-Sreenibha@users.noreply.github.com>
2019-06-13 11:13:32 +0530

    Add files via upload

---
+++ adminpg.php
@@ -0,0 +1,42 @@
+<HTML>
+<HEAD>
+<TITLE>chitchat</TITLE>
+</HEAD>
+<BODY bgcolor="#BF69D9">
+<p> 
+<font size = 6>A Xtreme Security Product </font>
+<br><br>
+<br><font size= 14><i><center>Chat Express - Administrative login</i></center></font></p>
+<p align="center"><b>This page is only meant for authorized users...</b></p>
+<br>
+<br>
+<form align="center" action="newadmin.php">
+<input type="submit" value="Create admin account">
+</form>
+<form action="verifyadm.php" method="post" align="right">
+admin-name:
+<input type="text" name="a_name" required/>
+<br>
+<br>
+password:
+<input type="password" name="a_pass" required/>
+<br>
+<br>
+<input type="submit" value="Submit" />
+</form>
+<p align="right">---or---</p>
+<form align="right" action="index.php">
+<input type="submit" value="Already an User">
+</form>
+<p align="right">---or---</p>
+<form align="right" action="signuppage.php">
+<input type="submit" value="Create an Account">
+</form>
+<br><br><br><br><br>
+
+<h4 align = "right">
+ -created by team Invictus
+</h4>
+
+</BODY>
+</HTML>
